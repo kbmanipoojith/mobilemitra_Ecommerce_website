5c13fa2a4d3f4369712e2a5556731294f86a87c7
Kb.Manipoojith <kbmanipoojith@gmail.com>
2025-04-12 20:39:17 +0530

    Fixed merge conflicts and updated codebase

--- backend/core/models.py
+++ backend/core/models.py
@@ -90,10 +90,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     model = models.ForeignKey(Model, on_delete=models.CASCADE, related_name='products')
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, related_name='products')
-<<<<<<< HEAD
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name='products', null=True)
-=======
->>>>>>> origin/master
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
