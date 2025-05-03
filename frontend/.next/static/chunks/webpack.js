607b0cf8fcda1626c23709d457038c44f4798cac
kbmanipoojith <kbmanipoojith@gmail.com>
2025-05-04 00:20:45 +0530

    updates and screenshots added

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2d7df5ab6bfd65a"; }
+/******/ 		__webpack_require__.h = function() { return "2d771f6a61389384"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
